82e2579c002ac3e705e9d80215ef09086a91e67d
Denis Shakurov <den_shak@mail.ru>
2025-02-28 16:24:35 +0300

    Version Code change to 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.den.shak.nds"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8
+        versionCode = 9
         versionName = "2.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
