00e409903f68ebe1ff0429ee2408a0c2cb012404
Denis Shakurov <den_shak@mail.ru>
2026-08-17 02:08:45 +0300

    Обновление versionCode до 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.den.shak.nds"
         minSdk = 24
         targetSdk = 37
-        versionCode = 10
+        versionCode = 11
         versionName = "2.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
